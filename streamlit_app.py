ded7af46da5965f050208eac6ca4517e7476daa4
Alicja Tchorz <68282882+ala-valerie@users.noreply.github.com>
2023-03-09 12:15:18 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Green Paper Mache toast')
+streamlit.text('🥑🍞 Green Paper Mache ttoast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
