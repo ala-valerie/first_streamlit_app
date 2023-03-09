2f45ee2c087b9eec712ce56e20787c2ae4cde5c7
Alicja Tchorz <68282882+ala-valerie@users.noreply.github.com>
2023-03-09 13:43:11 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,8 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
-streamlit.title('My Parents New Healthy Dinner') streamlit.header('Breakfast Menu')
+streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
